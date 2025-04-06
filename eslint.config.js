e46ae8d429ddf9cc85cb155159bee9ccee7a7a1b
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-04-05 21:05:13 -0300

    Create project with vite react-ts template, react router, tailwindcss and eslint

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import stylistic from '@stylistic/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  stylistic.configs.recommended,
+  stylistic.configs.customize({ semi: true }),
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylistic,
+  {
+    ignores: ['dist'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="m-auto max-w-sm text-center grid">
+      <h1>Olá, mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
